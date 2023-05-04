071e104ee50e6496248cabd44b8296e56563f3b3
Brandon Colangelo <brandoncancode@gmail.com>
2023-05-04 09:49:56 -0400

    Initial Commit

---
+++ index.py
@@ -0,0 +1,10 @@
+# Push all the odd numbers from 1 through a value given by a user into a new list
+
+def gettin_odd(num):
+    num = num+1
+    new_list = []
+    for i in range(1, num, 2):
+        new_list.append(i)
+    return new_list
+
+print(gettin_odd(10))
